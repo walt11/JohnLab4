22a2346e01f5aaee7f61e8042e84645313ca4066
walt11 <johnwalt011@gmail.com>
2016-09-25 00:12:39 -0500

    Cleaned up files, final version

--- lab4.c
+++ lab4.c
@@ -1,16 +1,31 @@
+/*
+##################################
+Name:		John Walter
+Date:		9/24/16
+Class:		ECE 3220
+Assignment:	Lab 4
+##################################
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
+//Global variables 
 int length = 0;
 double offset,scale;
+int max_value;
+
+// This function reads in the data from a file to an integer array and returns a pointer to the array
 int *Readfile(int file){
- 	int cell, max, i;
+ 	int cell, i;
  	int array[2000];
  	int *arrayPtr = array;
 	char file_name[20];
-	if(file<9){
+
+	//Formats the file name based on the number inputted by the user
+	if(file<9){ // includes a zero in the name
 		sprintf(file_name,"Raw_data_0%d.txt",file);
-	}else{
+	}else{ //doesn't include a zero in the name
 		sprintf(file_name,"Raw_data_%d.txt",file);
 	}
 	FILE *fp = fopen(file_name,"r");
@@ -18,7 +33,7 @@ int *Readfile(int file){
 		printf("Error opening file.\n");
 		return NULL;
 	}
-	fscanf(fp,"%d %d",&length,&max);
+	fscanf(fp,"%d %d",&length,&max_value);
 	for(i=0;i<length;i++){
 		fscanf(fp,"%d",&cell);
 		array[i] = cell;
@@ -28,57 +43,48 @@ int *Readfile(int file){
 	return arrayPtr;
 }
 
-void WriteFile(int file, int OorS, double *array){
-	char file_name[20];
+// This function writes the values in a double array to a file 
+void WriteFile(char file_name[20], double *array, double x){ // the x represents the offset or scale factor used in the transformations 
 	int i;
-	double x;
-	if(OorS == 1){
-		if(file<10){
-			sprintf(file_name,"Offset_data_0%d.txt",file);
-		}else{
-			sprintf(file_name,"Offset_data_%d.txt",file);
-		}
-		x = offset;
-	}
-	else{
-		if(file<10){
-			sprintf(file_name,"Scaled_data_0%d.txt",file);
-		}else{
-			sprintf(file_name,"Scaled_data_%d.txt",file);
-		}
-		x = scale;
-
-	}
+	
 	FILE *fp = fopen(file_name,"w");
 	if(fp == NULL){
 		printf("Error opening file\n");
 	}
-	fprintf(fp,"%d %f\n",length,x);
+	fprintf(fp,"%d %.4f\n",length,x);
 	for(i=0;i<length;i++){
 		fprintf(fp,"%.4f\n",array[i]);
 	}
 	fclose(fp);
 }
 
+// This function calculates the mean (average) of an integer array
 double mean(int *array){
 	double total = 0.0;
 	int i=0;
 	for(i=0;i<length;i++){
 		total = total+array[i];
 	}
-	return total/(double)length;
+	return total/(double)length; //length is type int
 }
 
+// This function returns the maximum possible value of the signal
 double max(int *array){
-	int i=0;
+	// Originally thought that this function needed to actually find the maximum value of the signal
+	// but then realized it just needs to return the max value given in the file, which is declared as
+	// a global variable above and found the the Readfile() function
+	/*int i=0;
 	double max = (double)array[i];
 	for(i=0;i<length;i++){
 		if(array[i] > max){max = array[i];}
 	}
 	return max;
+	*/
+	return (double)max_value;
 }
 
-void writeMeanMaxToFile(int *array, int file){
+// This function generates a file containing the mean and max of each raw file
+void createStatisticsFile(int *array, int file){
 	char file_name[20];
 	if(file<10){
 		sprintf(file_name,"Statistics_data_0%d.txt",file);
@@ -91,80 +97,98 @@ void writeMeanMaxToFile(int *array, int file){
 	fclose(fp);
 }
 
-void centerSignal(int *array, int file){
-	char file_name[20];
-	if(file<10){
-		sprintf(file_name,"Centered_data_0%d.txt",file);
-	}else{
-		sprintf(file_name,"Centered_data_%d.txt",file);
-	}
-	
-	FILE * fp = fopen(file_name,"w");
-	if(fp == NULL){
-		printf("File could not be opened\n");
-	}
+// This function offsets the signal based on an offset value inputted
+void offsetSignal(int *array, double offset_value, char file_name[20]){
 	int i;
-	fprintf(fp,"%d %.4f\n",length,mean(array));
+	double arrayD[2000];
 	for(i=0;i<length;i++){
-		fprintf(fp,"%.4f\n",(double)array[i]-mean(array));
+		arrayD[i] = (double)array[i] + offset_value;
 	}
-	fclose(fp);
-	
+	//Writes the new array to a file
+	WriteFile(file_name, arrayD, offset_value);
 }
 
-void normalizeSignal(int *array, int file){
-	char file_name[20];
-	if(file<10){
-		sprintf(file_name,"Normalized_data_0%d.txt",file);
-	}else{
-		sprintf(file_name,"Normalized_data_%d.txt",file);
-	}
-	
-	FILE * fp = fopen(file_name,"w");
-	
+//This function scales the signal based on a scale factor inputted
+void scaleSignal(int *array, double scale_factor, char file_name[20]){
 	int i;
-	fprintf(fp,"%d %.4f",length,mean(array));
+	double arrayD[2000];
 	for(i=0;i<length;i++){
-		fprintf(fp,"%.4f\n",(double)array[i]/max(array));
+		arrayD[i] = (double)array[i] * scale_factor;
 	}
-	fclose(fp);
-	
+	//Writes the new array to a file
+	WriteFile(file_name, arrayD, scale_factor);
 }
 
+//This function centers the signal 
+void centerSignal(int *array, char file_name[20]){
+	// Since centering is the same as offsetting the signal by a factor
+	// of -mean, the offsetSignal can be used with an offset value of -mean
+	offsetSignal(array, (-mean(array)), file_name);
+}
 
+// This function normalizes the singal
+void normalizeSignal(int *array, char file_name[20]){
+	// Since normalizing is the same as scaling the signal by a factor
+	// of 1/max, the scaleSignal() function can be used with a scale value of 1/max
+	scaleSignal(array, (1/max(array)), file_name);
+}
 
 int main(void){
-	double arrayD[2000];
-	int file, choice, i=0;
+	char file_name[20];
+	int file, choice;
 	printf("Enter file number: ");
 	scanf("%d",&file);
+	while((file < 1) | (file > 10)){
+		printf("Invalid file number: ");
+		scanf("%d",&file);
+	}
+	//Read file and create int array of data in file
 	int *array = Readfile(file);
 	printf("Select\n\t(1) Offset\n\t(2) Scale\n\t(3) Center\n\t(4) Normalize\n> ");
 	scanf("%d",&choice);
+	while((choice < 1) | (choice > 4)){
+		printf("Invalid choice. Try again: ");
+		scanf("%d",&choice);
+	}
 	switch(choice){
 		case 1:
+				if(file<10){
+					sprintf(file_name,"Offset_data_0%d.txt",file);
+				}else{
+					sprintf(file_name,"Offset_data_%d.txt",file);
+				}
 				printf("Enter offset value: ");
 				scanf("%lf",&offset);
-				for(i=0;i<length;i++){
-					arrayD[i] = (double)array[i] + offset;
-				}
+				offsetSignal(array, offset, file_name);
 				break;
 		case 2:
-				printf("Enter scaling value: ");
-				scanf("%lf",&scale);
-				for(i=0;i<length;i++){
-					arrayD[i] = (double)array[i] * scale;
+				if(file<10){
+					sprintf(file_name,"Scaled_data_0%d.txt",file);
+				}else{
+					sprintf(file_name,"Scaled_data_%d.txt",file);
 				}
+				printf("Enter scale factor: ");
+				scanf("%lf",&scale);
+				scaleSignal(array, scale, file_name);
 				break;
 		case 3:
-				centerSignal(array,file);
+				if(file<10){
+					sprintf(file_name,"Centered_data_0%d.txt",file);
+				}else{
+					sprintf(file_name,"Centered_data_%d.txt",file);
+				}
+				centerSignal(array,file_name);
 				break;
 		case 4:
-				normalizeSignal(array,file);
+				if(file<10){
+					sprintf(file_name,"Normalized_data_0%d.txt",file);
+				}else{
+					sprintf(file_name,"Normalized_data_%d.txt",file);
+				}
+				normalizeSignal(array,file_name);
 				break;
 	}
-	WriteFile(file,choice,arrayD);
-	writeMeanMaxToFile(array,file);
-
+	createStatisticsFile(array,file); //generate statistics file
 	return 0;
+	
 }
