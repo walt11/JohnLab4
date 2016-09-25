87b652dffd74e1a2f178196baa5a05db5e469943
walt11 <johnwalt011@gmail.com>
2016-09-24 23:59:07 -0500

    Final version

--- lab4.c
+++ lab4.c
@@ -33,7 +33,7 @@ void WriteFile(int file, int OorS, double *array){
 	int i;
 	double x;
 	if(OorS == 1){
-		if(file<9){
+		if(file<10){
 			sprintf(file_name,"Offset_data_0%d.txt",file);
 		}else{
 			sprintf(file_name,"Offset_data_%d.txt",file);
@@ -41,7 +41,7 @@ void WriteFile(int file, int OorS, double *array){
 		x = offset;
 	}
 	else{
-		if(file<9){
+		if(file<10){
 			sprintf(file_name,"Scaled_data_0%d.txt",file);
 		}else{
 			sprintf(file_name,"Scaled_data_%d.txt",file);
@@ -60,13 +60,86 @@ void WriteFile(int file, int OorS, double *array){
 	fclose(fp);
 }
 
+double mean(int *array){
+	double total = 0.0;
+	int i=0;
+	for(i=0;i<length;i++){
+		total = total+array[i];
+	}
+	return total/(double)length;
+}
+
+double max(int *array){
+	int i=0;
+	double max = (double)array[i];
+	for(i=0;i<length;i++){
+		if(array[i] > max){max = array[i];}
+	}
+	return max;
+}
+
+void writeMeanMaxToFile(int *array, int file){
+	char file_name[20];
+	if(file<10){
+		sprintf(file_name,"Statistics_data_0%d.txt",file);
+	}else{
+		sprintf(file_name,"Statistics_data_%d.txt",file);
+	}
+	
+	FILE * fp = fopen(file_name,"w");
+	fprintf(fp,"%.4f %.0f",mean(array),max(array));
+	fclose(fp);
+}
+
+void centerSignal(int *array, int file){
+	char file_name[20];
+	if(file<10){
+		sprintf(file_name,"Centered_data_0%d.txt",file);
+	}else{
+		sprintf(file_name,"Centered_data_%d.txt",file);
+	}
+	
+	FILE * fp = fopen(file_name,"w");
+	if(fp == NULL){
+		printf("File could not be opened\n");
+	}
+	int i;
+	fprintf(fp,"%d %.4f\n",length,mean(array));
+	for(i=0;i<length;i++){
+		fprintf(fp,"%.4f\n",(double)array[i]-mean(array));
+	}
+	fclose(fp);
+	
+}
+
+void normalizeSignal(int *array, int file){
+	char file_name[20];
+	if(file<10){
+		sprintf(file_name,"Normalized_data_0%d.txt",file);
+	}else{
+		sprintf(file_name,"Normalized_data_%d.txt",file);
+	}
+	
+	FILE * fp = fopen(file_name,"w");
+	
+	int i;
+	fprintf(fp,"%d %.4f",length,mean(array));
+	for(i=0;i<length;i++){
+		fprintf(fp,"%.4f\n",(double)array[i]/max(array));
+	}
+	fclose(fp);
+	
+}
+
+
+
 int main(void){
 	double arrayD[2000];
 	int file, choice, i=0;
 	printf("Enter file number: ");
 	scanf("%d",&file);
 	int *array = Readfile(file);
-	printf("Select\n\t(1) Offset\n\t(2) Scale\n> ");
+	printf("Select\n\t(1) Offset\n\t(2) Scale\n\t(3) Center\n\t(4) Normalize\n> ");
 	scanf("%d",&choice);
 	switch(choice){
 		case 1:
@@ -83,8 +156,15 @@ int main(void){
 					arrayD[i] = (double)array[i] * scale;
 				}
 				break;
+		case 3:
+				centerSignal(array,file);
+				break;
+		case 4:
+				normalizeSignal(array,file);
+				break;
 	}
 	WriteFile(file,choice,arrayD);
+	writeMeanMaxToFile(array,file);
 
 	return 0;
 }
